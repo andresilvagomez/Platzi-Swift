f47eff9aba99b0b216a81e8395ad8f2e30c1df55
Andres Silva <andresilvagomez@gmail.com>
2018-11-12 15:52:25 -0500

    Clase 23

--- PlatziFinanzas.playground/Contents.swift
+++ PlatziFinanzas.playground/Contents.swift
@@ -1,7 +1,15 @@
 import Foundation
 
-struct Acccount {
-    var amounth: Float = 0
+class Acccount {
+    var amounth: Float = 0 {
+        willSet {
+            print("Vamos a cambiar el valor", amounth, newValue)
+        }
+        didSet {
+            print("Tenemos nuevo valor", amounth)
+        }
+    }
+    
     var name: String = ""
     var transactions: [Float] = []
     
@@ -11,7 +19,7 @@ struct Acccount {
     }
     
     @discardableResult
-    mutating func addTransaction(value: Float) -> Float {
+    func addTransaction(value: Float) -> Float {
         if (amounth - value) < 0 {
             return 0
         }
@@ -23,13 +31,28 @@ struct Acccount {
     }
 }
 
-struct Person {
-    var name: String
-    var lastName: String
+class Person {
+    var name: String = ""
+    var lastName: String = ""
     var account: Acccount?
+    
+    var fullName: String {
+        get {
+            return "\(name) \(lastName)"
+        }
+        set {
+            name = String(newValue.split(separator: " ").first ?? "")
+            lastName = "\(newValue.split(separator: " ").last ?? "")"
+        }
+    }
+    
+    init(name: String, lastName: String) {
+        self.name = name
+        self.lastName = lastName
+    }
 }
 
-var me = Person(name: "Andres", lastName: "Silva", account: nil)
+var me = Person(name: "Andres", lastName: "Silva")
 
 let account = Acccount(amounth: 100_000, name: "X bank")
 
@@ -37,8 +60,15 @@ me.account = account
 
 print(me.account!)
 
+account.addTransaction(value: 20)
 me.account?.addTransaction(value: 20)
 
-print(me.account!)
+print(me.account!.amounth)
 
+print(me.fullName)
+
+me.fullName = "Pedro Perez"
+
+print(me.lastName)
+print(me.name)
 
