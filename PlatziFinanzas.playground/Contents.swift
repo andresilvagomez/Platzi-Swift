031281fa35b007494723479a71734791629f31b5
Andres Silva <andresilvagomez@gmail.com>
2018-11-12 15:32:29 -0500

    Clase 22

--- PlatziFinanzas.playground/Contents.swift
+++ PlatziFinanzas.playground/Contents.swift
@@ -3,6 +3,24 @@ import Foundation
 struct Acccount {
     var amounth: Float = 0
     var name: String = ""
+    var transactions: [Float] = []
+    
+    init(amounth: Float, name: String) {
+        self.amounth = amounth
+        self.name = name
+    }
+    
+    @discardableResult
+    mutating func addTransaction(value: Float) -> Float {
+        if (amounth - value) < 0 {
+            return 0
+        }
+        
+        amounth -= value
+        transactions.append(value)
+        
+        return amounth
+    }
 }
 
 struct Person {
@@ -16,6 +34,10 @@ var me = Person(name: "Andres", lastName: "Silva", account: nil)
 let account = Acccount(amounth: 100_000, name: "X bank")
 
 me.account = account
+
+print(me.account!)
+
+me.account?.addTransaction(value: 20)
 
 print(me.account!)
 
